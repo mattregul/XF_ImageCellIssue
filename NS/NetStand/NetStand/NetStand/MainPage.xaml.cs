3cc9924caa15ad65932634914151b25cbd62cdbc
Matt Regul <dev.matt@pachead.com>
2018-02-12 16:33:48 -0500

    first push

---
+++ NS/NetStand/NetStand/NetStand/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetStand
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+
+        public Employee(string _name, string _job)
+        {
+            Name = _name;
+            Job = _job;
+        }
+    }
+    public partial class MainPage : ContentPage
+	{
+        List<Employee> employeeList;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            employeeList = new List<Employee>();
+
+            // Create a few sample employees
+            for (int i = 0; i < 50; i++)
+            {
+                employeeList.Add(
+                    new Employee(
+                        "Employee " + i,
+                        "Developer"
+                    )
+                );
+            }
+
+            // Set the source
+            EmployeeListview.ItemsSource = employeeList;
+        }
+    }
+
+}
+
---
+++ PCL/XamarinForms_ListviewSelection-master/XamarinForms_ListviewSelection/ListviewTestPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+
+namespace XamarinForms_ListviewSelection
+{
+	
+	/// <summary>
+	/// Employee Class
+	/// </summary>
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+
+        public Employee(string _name, string _job)
+        {
+            Name = _name;
+            Job = _job;
+        }
+    }
+
+
+    public partial class ListviewTestPage : ContentPage
+	{
+		// our global employee list
+		List<Employee> employeeList;
+
+		public ListviewTestPage()
+		{
+            List<Employee> employeeList;
+
+            InitializeComponent();
+
+            employeeList = new List<Employee>();
+
+            // Create a few sample employees
+            for (int i = 0; i < 50; i++)
+            {
+                employeeList.Add(
+                    new Employee(
+                        "Employee " + i,
+                        "Developer"
+                    )
+                );
+            }
+
+            // Set the source
+            EmployeeListview.ItemsSource = employeeList;
+
+        }
+        
+	}
+}
+
